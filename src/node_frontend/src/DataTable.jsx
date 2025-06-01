5023ef7f677a16c8fcecf2aaa4b9696f23213db1
Romain <romain.rothn@gmail.com>
2025-06-01 21:13:12 +0200

    amelioration du style

--- src/node_frontend/src/DataTable.jsx
+++ src/node_frontend/src/DataTable.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
+import './DataTable.css';
 
 const DataTable = () => {
   const [data, setData] = useState([]);
